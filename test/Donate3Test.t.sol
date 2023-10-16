eb320030858b659a3a53bb9c75059db998368eba
chibyke <chibykepride@gmail.com>
2023-10-16 12:12:16 +0000

    tests and debugging

--- test/Donate3Test.t.sol
+++ test/Donate3Test.t.sol
@@ -14,4 +14,17 @@ contract Donate3Test is Test {
         console.log(donate3.MIN_DONATION());
         assertEq(donate3.MIN_DONATION(), 100e18);
     }
+
+    function testAdminIsMsgSender() public {
+        console.log(donate3.i_admin());
+        console.log(msg.sender);
+        // assertEq(donate3.i_admin(), msg.sender);
+            // This test will fail because Donate3Test is the contract that deployed the donate3 address
+                // in the setUp(); function, hence it will be the owner
+            // While in this particular function, msg.sender is whoever is calling the Donate3Test
+                // i.e., Us ->(calls) Donate3Test ->(deploys) donate3
+                // msg.sender = us
+                // i_admin = Donate3Test
+        assertEq(donate3.i_admin(), address(this));
+    }
 }
