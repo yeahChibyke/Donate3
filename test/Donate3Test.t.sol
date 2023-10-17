890d0d6c6a6206a6be86ee907b0297d2a376ba8d
Replit user <>
2023-10-17 23:48:45 +0000

    chore: forge init

--- test/Donate3Test.t.sol
+++ test/Donate3Test.t.sol
@@ -6,6 +6,7 @@ import {Donate3} from "../src/Donate3.sol";
 
 contract Donate3Test is Test {
     Donate3 donate3;
+
     function setUp() external {
         donate3 = new Donate3();
     }
@@ -19,12 +20,12 @@ contract Donate3Test is Test {
         console.log(donate3.i_admin());
         console.log(msg.sender);
         // assertEq(donate3.i_admin(), msg.sender);
-            // This test will fail because Donate3Test is the contract that deployed the donate3 address
-                // in the setUp(); function, hence it will be the owner
-            // While in this particular function, msg.sender is whoever is calling the Donate3Test
-                // i.e., Us ->(calls) Donate3Test ->(deploys) donate3
-                // msg.sender = us
-                // i_admin = Donate3Test
+        // This test will fail because Donate3Test is the contract that deployed the donate3 address
+        // in the setUp(); function, hence it will be the owner
+        // While in this particular function, msg.sender is whoever is calling the Donate3Test
+        // i.e., Us ->(calls) Donate3Test ->(deploys) donate3
+        // msg.sender = us
+        // i_admin = Donate3Test
         assertEq(donate3.i_admin(), address(this));
     }
 }
