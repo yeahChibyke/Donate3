9e9bfc149ee0d657d66a53e8ce57a5390e509e7e
Replit user <>
2023-10-19 14:05:36 +0000

    ran forks 🍴

--- test/Donate3LibraryTest.t.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >= 0.6.0 < 0.9.0;
-
-import {Test} from "forge-std/Test.sol";
-import {Donate3Library} from "../src/Donate3Library.sol";
-
-contract Donate3LibraryTest is Test {}
